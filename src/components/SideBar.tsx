c94b56ffb01e22744c0ca9d23f51517d24109ac2
Pablo Santos <pablo.1999@alunos.utfpr.edu.br>
2021-12-09 08:20:52 -0300

    feat: finish challenge

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,31 @@
-export function SideBar() {
-  // Complete aqui
+import { GenreResponseProps } from "../App";
+import { Button } from "./Button";
+import "../styles/sidebar.scss";
+
+type SideBarProps = {
+  genres: GenreResponseProps[];
+  click: (id: number) => void;
+  selectedGenreId: number;
+};
+
+export function SideBar({ genres, click, selectedGenreId }: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => click(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
